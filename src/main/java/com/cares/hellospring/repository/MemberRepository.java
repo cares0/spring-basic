9b9409954c466e63d7d70cafc0649d83def3c146
cares0 <cares00000@gmail.com>
2022-04-12 21:26:17 +0900

    learn repository, service, testcase

---
+++ src/main/java/com/cares/hellospring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.cares.hellospring.repository;
+
+import com.cares.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
---
+++ src/main/java/com/cares/hellospring/service/MemberService.java
@@ -0,0 +1,48 @@
+package com.cares.hellospring.service;
+
+import com.cares.hellospring.domain.Member;
+import com.cares.hellospring.repository.MemberRepository;
+import com.cares.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    // 서비스는 비즈니스로직에 맞도록 메서드명을 작성함
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+    // 회원 가입
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원 X
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // Optional이 제공하는 메서드를 통해 해결 가능
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> { // 해당하는 값이 있다면 실행
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // 회원 한명 조회
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+
+}
