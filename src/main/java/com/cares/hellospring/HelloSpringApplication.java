90b364096307af6c308edbed2fa2861b120e773d
cares0 <cares00000@gmail.com>
2022-04-12 14:48:28 +0900

    learn MVC and Template Engine

--- src/main/java/com/cares/hellospring/HelloSpringApplication.java
+++ src/main/java/com/cares/hellospring/HelloSpringApplication.java
@@ -2,6 +2,7 @@ package com.cares.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class HelloSpringApplication {
@@ -9,5 +10,7 @@ public class HelloSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
+
+
 
 }
