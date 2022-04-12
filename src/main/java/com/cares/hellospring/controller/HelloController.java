7778255d733ad033435f0438c7cdc78a0fd3c60b
cares0 <cares00000@gmail.com>
2022-04-12 15:27:49 +0900

    learn API

--- src/main/java/com/cares/hellospring/controller/HelloController.java
+++ src/main/java/com/cares/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -20,4 +21,29 @@ public class HelloController {
         return "hello-template";
     }
 
+    @GetMapping("hello-string")
+    @ResponseBody // HTTP 프로토콜의 Body에 메서드의 리턴값을 담아서 보내주게된다.
+    public String helloString(String name) {
+        return "hello " + name; // View가 아니라, 그냥 이 문자 그대로 Body에 담겨 보낸다.
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
