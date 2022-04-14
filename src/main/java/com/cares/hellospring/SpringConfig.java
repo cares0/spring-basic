37bd8b9ca1382e24ee2ee35fcfc0ec03fd37f290
cares0 <cares00000@gmail.com>
2022-04-14 23:10:28 +0900

    learn JdbcConnection, OCP

--- src/main/java/com/cares/hellospring/SpringConfig.java
+++ src/main/java/com/cares/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.cares.hellospring;
 
+import com.cares.hellospring.repository.JdbcMemberRepository;
 import com.cares.hellospring.repository.MemberRepository;
 import com.cares.hellospring.repository.MemoryMemberRepository;
 import com.cares.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return  new MemoryMemberRepository();
+        return  new JdbcMemberRepository(dataSource);
     }
 }
